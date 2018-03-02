32f2a1b8728c8b9f387e2395fa28503c6494fb40
Daniele Fiori <d.fiori@fadeout.it>
2018-03-02 11:31:59 +0100

    Initial development of filters panel. Initial development of a dropdown element with advanced features (multiple selection, search, etc.)

--- src/Cimplicity.Views.WebClient/typings/index.d.ts
+++ src/Cimplicity.Views.WebClient/typings/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/moment/index.d.ts" />
 /// <reference path="globals/vex-js/index.d.ts" />
-///<reference path="globals/moment/index.d.ts"/>
