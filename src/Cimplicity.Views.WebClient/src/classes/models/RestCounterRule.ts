ac913bd52597f1da5709e4b31260259b14f50f51
jacopozzo <jacopo.gazzolo@gmail.com>
2018-03-09 14:16:28 +0100

    RestCounterRule object for SamplingExecutionService RestTimingRule object for SamplingExecutionService

---
+++ src/Cimplicity.Views.WebClient/src/classes/models/RestCounterRule.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../interfaces/irestcounterrule.ts" />
+module ge.cim.models {
+    import IRestCounterRule = ge.cim.IRestCounterRule;
+
+    export class RestCounterRule implements IRestCounterRule {
+        constructor(ruleExecutionId: string) {
+            this.RuleExecutionId = ruleExecutionId;
+        }
+        Counter: number;
+        RuleExecutionId: string;
+        Set?: number;
+        Actual?: number;
+
+    }
+}
+
---
+++ src/Cimplicity.Views.WebClient/src/classes/models/RestTimingRule.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="../../interfaces/iresttimingrule.ts" />
+
+module ge.cim.models {
+    import IRestTimingRule = ge.cim.IRestTimingRule;
+
+    export class RestTimingRule implements IRestTimingRule {
+        constructor(ruleExecutionId: string) {
+            this.RuleExecutionId = ruleExecutionId;
+        }
+        Seconds: number;
+        LastSamplingDate: Date;
+        RuleExecutionId: string;
+        Set?: number;
+        Actual?: string;
+    }
+}
